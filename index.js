d3d04e6a7bc65f3161ffffb9ae5d65a2f88695df
Araf <arafsoloman3734@gmail.com>
2023-05-15 22:03:03 +0600

    Javascript canvas api app

---
+++ index.js
@@ -0,0 +1,225 @@
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
+
+const cellsHorizontal = 14;
+const cellsVertical = 10;
+const width = window.innerWidth;
+const height = window.innerHeight;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
+
+const engine = Engine.create();
+engine.world.gravity.y = 0;
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        wireframes: false,
+        width,
+        height
+    }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+const walls = [
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
+];
+
+World.add(world, walls);
+
+// Maze generation
+
+const shuffle = arr => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+
+    return arr;
+};
+
+const grid = Array(cellsVertical)
+    .fill(null)
+    .map(() => Array(cellsHorizontal).fill(false));
+
+const verticals = Array(cellsVertical)
+    .fill(null)
+    .map(() => Array(cellsHorizontal - 1).fill(false));
+
+const horizontals = Array(cellsVertical - 1)
+    .fill(null)
+    .map(() => Array(cellsHorizontal).fill(false));
+
+const startRow = Math.floor(Math.random() * cellsHorizontal);
+const startColumn = Math.floor(Math.random() * cellsVertical);
+
+const stepThroughCell = (row, column) => {
+    // IF I have visited this cell before then return
+    if (grid[row][column]) {
+        return;
+    }
+
+    // Mark this cell being visited
+    grid[row][column] = true;
+
+    // Assemble randomly ordered list of neighbours
+    const neighbours = shuffle([
+        [row - 1, column, 'up'],
+        [row, column - 1, 'left'],
+        [row + 1, column, 'down'],
+        [row, column + 1, 'right']
+    ]);
+
+    // For each neighbour....
+    for (let neighbour of neighbours) {
+        const [nextRow, nextColumn, direction] = neighbour;
+        // See if that neighbour is out of bounds
+        if (nextRow < 0 ||
+            nextColumn < 0 ||
+            nextRow >= cellsVertical ||
+            nextColumn >= cellsHorizontal
+        ) {
+            continue;
+        }
+        // If we have visited that neighbour continue to next neighbour
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+        // Remove a wall from either verticals or horizontals
+        if (direction === 'left') {
+            verticals[row][column - 1] = true;
+        } else if (direction === 'right') {
+            verticals[row][column] = true;
+        } else if (direction === 'up') {
+            horizontals[row - 1][column] = true;
+        } else if (direction === 'down') {
+            horizontals[row][column] = true;
+        }
+        stepThroughCell(nextRow, nextColumn);
+    }
+
+    // Visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * unitLengthX + unitLengthX / 2,
+            rowIndex * unitLengthY + unitLengthY,
+            unitLengthX,
+            5,
+            {
+                isStatic: true,
+                label: 'wall',
+                render: {
+                    fillStyle: 'red'
+                }
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+        const wall = Bodies.rectangle(
+            columnIndex * unitLengthX + unitLengthX,
+            rowIndex * unitLengthY + unitLengthY / 2,
+            5,
+            unitLengthY,
+            {
+                isStatic: true,
+                label: 'wall',
+                render: {
+                    fillStyle: 'red'
+                }
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+// Goal
+
+const goal = Bodies.rectangle(
+    width - unitLengthX / 2,
+    height - unitLengthY / 2,
+    unitLengthX * 0.7,
+    unitLengthY * 0.7,
+    {
+        isStatic: true,
+        label: 'goal',
+        render: {
+            fillStyle: 'green'
+        }
+    }
+);
+// Ball
+
+const ball = Bodies.circle(
+    unitLengthX / 2,
+    unitLengthY / 2,
+    unitLengthY * 0.25,
+    {
+        label: 'ball',
+        render: {
+            fillStyle: 'blue'
+        }
+    }
+);
+
+// Adding goal and ball
+World.add(world, goal);
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const { x, y } = ball.velocity;
+
+    if (event.key === 'w') {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+    if (event.key === 'd') {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+    if (event.key === 's') {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+    if (event.key === 'a') {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+});
+
+// Win Condition
+
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach((collision) => {
+        const labels = ['ball', 'goal'];
+        if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+            document.querySelector('.winner').classList.remove('hidden');
+            world.gravity.y = 1;
+            world.bodies.forEach(body => {
+                if (body.label === 'wall') {
+                    Body.setStatic(body, false);
+                }
+            });
+        }
+    });
+});
---
+++ readme.txt
@@ -0,0 +1,11 @@
+1. Implemented a game with the matters library of javascript.
+
+2. It mainly involved working with many attributes and methods of the matters library.
+
+3. Made use of many array methods of javascript and other kinds of programming stuffs in javascript.
+
+4. Did work with the event listener from the keyboard.
+
+5. Involved a little bit of DoM manupilation in it.
+
+6. An algorithm was introduced in it, that is almost similar to the DFS, so that the maze of the game can be created.
